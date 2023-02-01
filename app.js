7d54110d0584da62ea5d7cf6d6c349fa69402d8b
Burak Civelek <brkcvlk15@gmail.com>
2023-02-02 02:36:22 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function AnaSayfa() {
+    window.location.href = 'index.html';
+}
+
+function Hakkimizda() {
+    window.location.href = 'Hakkimizda.html'
+}
+
+function Ürünlerimiz() {
+    window.location.href = 'Ürünlerimiz.html'
+}
